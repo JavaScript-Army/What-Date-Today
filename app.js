f3f2bc2cb7dcfef37d196b02af47f79b5b0e1d15
Ashraful Oli <ashrafuloli@Ashrafuls-MacBook-Pro.local>
2022-10-10 10:29:57 +0600

    problem solved

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-//It's help me a lot, because I forget what date continuing Today 
+//It's help me a lot, because I forget what date continuing Today
+
+function knowDate() {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const date = new Date();
+  return alert(days[date.getDay()]);
+}
